39a105f33308c559fae20edf0d1a83efc30a958c
mkodekar <rkodekar@zoho.com>
2016-10-22 02:46:30 +0530

    adding data annotation to show how it actually works, see the output in data_annotation.png

--- src/mykotlinclasses/dataclasses/People.kt
+++ src/mykotlinclasses/dataclasses/People.kt
@@ -6,4 +6,9 @@ package mykotlinclasses.dataclasses
 // this is a normal data clas in kotlin which is exactly
 // what plain old java or pojo class is which has various getters
 // and setters in java, in kotlin its just one line
-class People (var id: Int, var name: String, var email: String)
+
+/*
+ * part 2 , data annotation.
+ */
+// lets just annotate this with data annotation to see the effect
+data class People (var id: Int, var name: String, var email: String)
