9dacb43a25619cf87cf933933b8e4500f796050a
mkodekar <rkodear@zoho.com>
2016-10-22 02:38:42 +0530

    comment most of the stuffs to explain code

--- src/mykotlinclasses/MainWorkspace.kt
+++ src/mykotlinclasses/MainWorkspace.kt
@@ -6,6 +6,9 @@ import mykotlinclasses.dataclasses.People
  * Created by rkodekar on 10/22/16.
  */
 fun main(args: Array<String>) {
+    // lets  say i have declared the data class here ,  -- > if you are refering to this class first , read the people.kt class in dataclass folder and then read this.
+    // twice because of comparing it, see the next commit for what actually happens
+    // refer the output for this , named as without_annotation
     val people: People = People(1, "Rehan Kodekar", "mkodekar@zoho.com")
     val people1: People = People(1, "Rehan Kodekar", "mkodekar@zoho.com")
 
--- src/mykotlinclasses/dataclasses/People.kt
+++ src/mykotlinclasses/dataclasses/People.kt
@@ -3,4 +3,7 @@ package mykotlinclasses.dataclasses
 /**
  * Created by rkodekar on 10/22/16.
  */
+// this is a normal data clas in kotlin which is exactly
+// what plain old java or pojo class is which has various getters
+// and setters in java, in kotlin its just one line
 class People (var id: Int, var name: String, var email: String)
